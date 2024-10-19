36ce42afdde07a832288b4fa4c2c1e4363646ada
tridominh <tridmse173029@fpt.edu.vn>
2024-10-19 16:29:31 +0700

    fix notification

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
+import unreadNotifySlice from '@/reducers/unreadNotifySlice';
 import tokenReducer from '../reducers/tokenSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
-        token: tokenReducer
+        token: tokenReducer,
+        unreadNotify: unreadNotifySlice
     }
 });
 export type RootState = ReturnType<typeof store.getState>;
